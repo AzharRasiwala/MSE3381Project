0831cd515a01e5d8fed779d26454a9717de24c83
JD <jherlehy@uwo.ca>
2021-12-05 12:46:41 -0500

    MatLab

---
+++ Spain_without_the_s.m
@@ -0,0 +1,24 @@
+syms r1 r2 r3 r4 v1 v2 v3 v4 a1 a2 a3 a4 th1 th2 th3 th4 w1 w2 w3 w4 al1 al2 al3 al4
+
+th2 = linspace(0, 720, 1000)
+
+w2 = 10.47
+
+th3 = acosd((0.02 - 0.2*cosd(th2))/(1.2))
+r4 = -(0.2*sind(th2) + 1.2*sind(th3))
+
+w3 = (-0.2*sind(th2)*w2)/(1.2*sind(th3))
+v4 = -(0.2*cosd(th2)*w2 + 1.2*cosd(th3)*w3)
+
+al3 = (-0.2*cosd(th2)*w2^2)/(1.2*sind(th3))
+a4  = -(1.2*cosd(th3)*al3 - 1.2*sind(th3)*w3^2 - 0.2*sind(th2)*w2^2)
+
+plot(th2, r4)
+figure()
+plot(th2, v4)
+figure()
+plot(th2, a4)
+
+writematrix(r4, 'Position.csv')
+writematrix(v4, 'Velocity.csv')
+writematrix(a4, 'Aceleration.csv')
